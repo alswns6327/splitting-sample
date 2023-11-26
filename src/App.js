4e147cf32847d13cacd76d92d3fac231ace1d7f1
alswns6327 <alswnsdl333@gmail.com>
2023-11-26 22:22:04 +0900

    state split component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import notify from './notify';
+import { Component } from 'react';
+// import SplitMe from './SplitMe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={() => import('./notify').then(result => result.notify2())}>
-        {/* <p onClick={() => notify()}> */}
-          Hello React
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    SplitMe : null
+  }
+
+  render(){
+    const {SplitMe} = this.state;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <p onClick={() => import('./notify').then(result => result.notify2())}> */}
+          <p onClick={async () => {
+            const loadModule = await (await import('./SplitMe'))
+            this.setState({SplitMe : loadModule.default})
+          }}>
+          {/* <p onClick={() => notify()}> */}
+            Hello React
+          </p>
+          {SplitMe && <SplitMe/>}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
