afc70b35f26219914f633c5fa1052b9dc96b68ed
alswns6327 <alswnsdl333@gmail.com>
2023-11-28 23:35:22 +0900

    React lazy 코드 스플릿

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import notify from './notify';
-import { Component } from 'react';
-// import SplitMe from './SplitMe';
+import React, { Suspense, useState } from 'react';
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
-class App extends Component {
+function App (){
+    const [visible, setVisible] = useState(false);
 
-  state = {
-    SplitMe : null
-  }
+    const onClick = () =>{
+      setVisible(true);
+    }
 
-  render(){
-    const {SplitMe} = this.state;
-    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {/* <p onClick={() => import('./notify').then(result => result.notify2())}> */}
-          <p onClick={async () => {
-            const loadModule = await (await import('./SplitMe'))
-            this.setState({SplitMe : loadModule.default})
-          }}>
-          {/* <p onClick={() => notify()}> */}
-            Hello React
-          </p>
-          {SplitMe && <SplitMe/>}
+          <p onClick={onClick}>Hello React</p>
+          <Suspense fallback={<div>loading...</div>}>
+            {visible && <SplitMe/>}
+          </Suspense>
         </header>
       </div>
     );
-  }
 }
 
 export default App;
